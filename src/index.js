9cb87d76477b2c30c2ac4db61bd1d42c1e92dbad
abhi114 <nabhi531@gmail.com>
2025-07-20 19:49:41 +0530

    cloudinary implemented and express upload middleware added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import {clerkMiddleware} from '@clerk/express';
+import fileUpload from 'express-fileupload';
 import userRoutes from './routes/user.route.js';
 import adminRoutes from './routes/admin.route.js';
 import authRoutes from './routes/auth.route.js';
@@ -11,8 +13,17 @@ import { connectDb } from './lib/db.js';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 app.use(express.json()); // to parse JSON bodies
 app.use(clerkMiddleware()) //this will add the auth to req object => req.auth.userId this will give us the userId of the logged in user
+// this is the directory where the uploaded files will be stored temporarily until they are uploaded to cloudinary
+//always put this before the routes so that it can be implemented before the routes are setup
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir:path.join(__dirname,'tmp'),
+    createParentPath:true, // this will create the parent directory if it does not exist
+    limits:{fileSize:10*1024*1024} // this will limit the file size to 10MB
+}))
 //console.log("Environment Variables:", process.env.PORT);
 app.use("/api/users",userRoutes);
 app.use("/api/auth", authRoutes);
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { User } from '../models/user.model';
+import { User } from '../models/user.model.js';
 import { authCallback } from '../controllers/auth.controller.js';
 const router = Router();
 
